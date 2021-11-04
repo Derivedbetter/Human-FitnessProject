fbd0bdb4b5ec5b04c2f1948b2a68830207b4665b
Ronald Hickman <93354976+Derivedbetter@users.noreply.github.com>
2021-11-04 17:16:34 -0400

    Add project files.

---
+++ Human FitnessProject/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace Human_FitnessProject
+{
+    partial class welcomePage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(welcomePage));
+            this.squareLogo = new System.Windows.Forms.PictureBox();
+            this.longLogo = new System.Windows.Forms.PictureBox();
+            this.squareLogo2 = new System.Windows.Forms.PictureBox();
+            this.welcomeLabel = new System.Windows.Forms.Label();
+            this.startLabel = new System.Windows.Forms.Label();
+            this.answer1 = new System.Windows.Forms.TextBox();
+            this.enterAnswer = new System.Windows.Forms.Label();
+            this.bcpicture = new System.Windows.Forms.PictureBox();
+            this.sameGuy = new System.Windows.Forms.Label();
+            this.sameShirt = new System.Windows.Forms.Label();
+            this.difMan = new System.Windows.Forms.Label();
+            this.couldbeYou = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.squareLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.longLogo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.squareLogo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bcpicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // squareLogo
+            // 
+            this.squareLogo.Image = ((System.Drawing.Image)(resources.GetObject("squareLogo.Image")));
+            this.squareLogo.Location = new System.Drawing.Point(23, 12);
+            this.squareLogo.Name = "squareLogo";
+            this.squareLogo.Size = new System.Drawing.Size(172, 172);
+            this.squareLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.squareLogo.TabIndex = 0;
+            this.squareLogo.TabStop = false;
+            // 
+            // longLogo
+            // 
+            this.longLogo.Image = ((System.Drawing.Image)(resources.GetObject("longLogo.Image")));
+            this.longLogo.Location = new System.Drawing.Point(561, 630);
+            this.longLogo.Name = "longLogo";
+            this.longLogo.Size = new System.Drawing.Size(524, 111);
+            this.longLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.longLogo.TabIndex = 1;
+            this.longLogo.TabStop = false;
+            // 
+            // squareLogo2
+            // 
+            this.squareLogo2.Image = ((System.Drawing.Image)(resources.GetObject("squareLogo2.Image")));
+            this.squareLogo2.Location = new System.Drawing.Point(1402, 12);
+            this.squareLogo2.Name = "squareLogo2";
+            this.squareLogo2.Size = new System.Drawing.Size(172, 172);
+            this.squareLogo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.squareLogo2.TabIndex = 2;
+            this.squareLogo2.TabStop = false;
+            // 
+            // welcomeLabel
+            // 
+            this.welcomeLabel.AutoSize = true;
+            this.welcomeLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.welcomeLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.welcomeLabel.Location = new System.Drawing.Point(479, 133);
+            this.welcomeLabel.Name = "welcomeLabel";
+            this.welcomeLabel.Size = new System.Drawing.Size(606, 51);
+            this.welcomeLabel.TabIndex = 3;
+            this.welcomeLabel.Text = "Welcome to the Human Fitness Project!";
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startLabel.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.startLabel.Location = new System.Drawing.Point(440, 184);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(672, 51);
+            this.startLabel.TabIndex = 4;
+            this.startLabel.Text = "Are you ready to begin your fitness Journey?";
+            // 
+            // answer1
+            // 
+            this.answer1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.answer1.Location = new System.Drawing.Point(685, 299);
+            this.answer1.Name = "answer1";
+            this.answer1.Size = new System.Drawing.Size(268, 35);
+            this.answer1.TabIndex = 5;
+            // 
+            // enterAnswer
+            // 
+            this.enterAnswer.AutoSize = true;
+            this.enterAnswer.Font = new System.Drawing.Font("Monotype Corsiva", 14F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enterAnswer.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.enterAnswer.Location = new System.Drawing.Point(229, 235);
+            this.enterAnswer.Name = "enterAnswer";
+            this.enterAnswer.Size = new System.Drawing.Size(1045, 51);
+            this.enterAnswer.TabIndex = 6;
+            this.enterAnswer.Text = "Type Yes to maximize your fitness potential or No to remain the same.";
+            // 
+            // bcpicture
+            // 
+            this.bcpicture.Image = ((System.Drawing.Image)(resources.GetObject("bcpicture.Image")));
+            this.bcpicture.Location = new System.Drawing.Point(12, 392);
+            this.bcpicture.Name = "bcpicture";
+            this.bcpicture.Size = new System.Drawing.Size(372, 372);
+            this.bcpicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bcpicture.TabIndex = 7;
+            this.bcpicture.TabStop = false;
+            // 
+            // sameGuy
+            // 
+            this.sameGuy.AutoSize = true;
+            this.sameGuy.Font = new System.Drawing.Font("Monotype Corsiva", 10F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sameGuy.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.sameGuy.Location = new System.Drawing.Point(414, 392);
+            this.sameGuy.Name = "sameGuy";
+            this.sameGuy.Size = new System.Drawing.Size(125, 37);
+            this.sameGuy.TabIndex = 8;
+            this.sameGuy.Text = "Same Guy";
+            // 
+            // sameShirt
+            // 
+            this.sameShirt.AutoSize = true;
+            this.sameShirt.Font = new System.Drawing.Font("Monotype Corsiva", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sameShirt.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.sameShirt.Location = new System.Drawing.Point(413, 440);
+            this.sameShirt.Name = "sameShirt";
+            this.sameShirt.Size = new System.Drawing.Size(159, 44);
+            this.sameShirt.TabIndex = 9;
+            this.sameShirt.Text = "Same Shirt";
+            // 
+            // difMan
+            // 
+            this.difMan.AutoSize = true;
+            this.difMan.Font = new System.Drawing.Font("Monotype Corsiva", 14F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.difMan.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.difMan.Location = new System.Drawing.Point(412, 497);
+            this.difMan.Name = "difMan";
+            this.difMan.Size = new System.Drawing.Size(420, 51);
+            this.difMan.TabIndex = 10;
+            this.difMan.Text = "Totally Different Person!";
+            // 
+            // couldbeYou
+            // 
+            this.couldbeYou.AutoSize = true;
+            this.couldbeYou.Font = new System.Drawing.Font("Monotype Corsiva", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.couldbeYou.ForeColor = System.Drawing.Color.Crimson;
+            this.couldbeYou.Location = new System.Drawing.Point(411, 548);
+            this.couldbeYou.Name = "couldbeYou";
+            this.couldbeYou.Size = new System.Drawing.Size(396, 57);
+            this.couldbeYou.TabIndex = 11;
+            this.couldbeYou.Text = "It\'s Your Turn Now!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1202, 392);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(372, 372);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
+            // 
+            // welcomePage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1582, 728);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.couldbeYou);
+            this.Controls.Add(this.difMan);
+            this.Controls.Add(this.sameShirt);
+            this.Controls.Add(this.sameGuy);
+            this.Controls.Add(this.bcpicture);
+            this.Controls.Add(this.enterAnswer);
+            this.Controls.Add(this.answer1);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.welcomeLabel);
+            this.Controls.Add(this.squareLogo2);
+            this.Controls.Add(this.longLogo);
+            this.Controls.Add(this.squareLogo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1614, 807);
+            this.Name = "welcomePage";
+            this.Text = "Welcome Page";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.welcomePage_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.squareLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.longLogo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.squareLogo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bcpicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox squareLogo;
+        private System.Windows.Forms.PictureBox longLogo;
+        private System.Windows.Forms.PictureBox squareLogo2;
+        private System.Windows.Forms.Label welcomeLabel;
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.TextBox answer1;
+        private System.Windows.Forms.Label enterAnswer;
+        private System.Windows.Forms.PictureBox bcpicture;
+        private System.Windows.Forms.Label sameGuy;
+        private System.Windows.Forms.Label sameShirt;
+        private System.Windows.Forms.Label difMan;
+        private System.Windows.Forms.Label couldbeYou;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Human FitnessProject/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Human_FitnessProject
+{
+    public partial class welcomePage : Form
+    {
+        public welcomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void welcomePage_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
